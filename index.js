b22487181a632655b385194924246531cd704065
Vince Paul Liem <vincepaul.liem@gmail.com>
2020-10-22 10:39:35 +0200

    babe is so fine<33

--- index.js
+++
@@ -1,3 +0,0 @@
-export function handleCsvFiles(files) {
-  return files
-}
---
+++ index.ts
@@ -0,0 +1,41 @@
+import { Fn, pipe, toggleLogging as togglePipeLogging } from '@veighsnche/pipe'
+
+export let logging = false
+
+const logLength = (name: string, subj: any[]) => {
+  logging && console.log(name, subj.length)
+  return subj
+}
+
+const readingTexts = (file: File) => new Promise(resolve => {
+  const reader = new FileReader()
+  reader.onload = event => resolve(event?.target?.result)
+  reader.readAsText(file)
+})
+
+// todo: make promisedMap
+const map = (fn: Fn) => (subj: any) => subj.map(fn)
+
+const allowMimeTypes = (types: string[]) => (files: File[]): File[] => files.filter(file => types.includes(file.type))
+
+const removeFalsy = (array: any[]): any[] => logLength('after removeFalsy: ', logLength('before removeFalsy: ', array).filter(Boolean))
+
+const hydrateFiles = (csv: FileList, files: File[] = [], i = 0): File[] => csv.item(i)
+  ? hydrateFiles(csv, [...files, csv.item(i) as File], i + 1)
+  : files
+
+const handlePipe = pipe(
+  hydrateFiles,
+  removeFalsy,
+  allowMimeTypes(['text/csv', 'application/vnd.ms-excel']),
+  map(readingTexts)
+)
+
+export function handleCsvFiles(files: FileList) {
+  return handlePipe(files)
+}
+
+export const toggleLogging = (): void => {
+  logging = !logging
+  togglePipeLogging()
+}
